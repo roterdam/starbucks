307d12140a3adff8bfd53f76c2fa4cfc99493dba
Saif Hakim <saif@mit.edu>
2012-03-01 15:36:14 -0500

    Work?

---
+++ src/edu/mit/compilers/grammar/DecafAST.java
@@ -0,0 +1,24 @@
+package edu.mit.compilers.grammar;
+
+import antlr.CommonAST;
+import antlr.Token;
+
+public class DecafAST extends CommonAST {
+	private int line = 0;
+	private int col = 0;
+	
+	@Override
+	public void initialize(Token t){
+		super.initialize(t);
+		line = t.getLine();
+		col = t.getColumn();
+	}
+	@Override
+	public int getLine(){
+		return line;
+	}
+	@Override
+	public int getColumn(){
+		return col;
+	}
+}
