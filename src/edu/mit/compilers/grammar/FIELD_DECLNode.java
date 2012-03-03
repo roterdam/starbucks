9b58fbe5feb629724f6ab239cb3e251b435f3524
Joshua Ma <me@joshma.com>
2012-03-03 13:55:54 -0500

    moar nodes added and fieldnode removed

---
+++ src/edu/mit/compilers/grammar/FIELD_DECLNode.java
@@ -0,0 +1,6 @@
+package edu.mit.compilers.grammar;
+
+
+public class FIELD_DECLNode extends DecafNode {
+
+}
---
+++ src/edu/mit/compilers/grammar/METHOD_RETURNNode.java
@@ -0,0 +1,6 @@
+package edu.mit.compilers.grammar;
+
+
+public class METHOD_RETURNNode extends DecafNode {
+
+}
