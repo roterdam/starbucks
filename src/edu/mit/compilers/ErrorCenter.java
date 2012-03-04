e7bd6c9364881aa07a2947e34adb6d5546cbdd67
Joshua Ma <me@joshma.com>
2012-03-04 18:15:43 -0500

    fixed tab issue in errors

--- src/edu/mit/compilers/ErrorCenter.java
+++ src/edu/mit/compilers/ErrorCenter.java
@@ -12,6 +12,9 @@ public class ErrorCenter {
 	static List<Long> lineOffsets;
 	static int maxLineNumberWidth;
 	static boolean hasError = false;
+	// This has to match with ANTLR's size, unfortunately.
+	static int ANTLR_TAB_SIZE = 8;
+	// This is OUR tab size. Much nicer.
 	static int TAB_SIZE = 4;
 
 	/**
@@ -26,7 +29,7 @@ public class ErrorCenter {
 			filename = newFilename;
 			lineOffsets = new ArrayList<Long>();
 			// Line "0" doesn't exist.
-			lineOffsets.add(-1L);
+			lineOffsets.add(0L);
 			lineOffsets.add(file.getFilePointer());
 			while (file.readLine() != null) {
 				lineOffsets.add(file.getFilePointer());
@@ -46,6 +49,7 @@ public class ErrorCenter {
 					+ ": " + message);
 			file.seek(lineOffsets.get(line));
 
+			// Generate the prefix before the line of code.
 			StringBuilder prefixSb = new StringBuilder();
 			for (int i = 0; i < maxLineNumberWidth
 					- Integer.toString(line).length(); i++) {
@@ -53,6 +57,7 @@ public class ErrorCenter {
 			}
 			prefixSb.append(line);
 			prefixSb.append(":");
+			// Combine the prefix and the code.
 			String out = prefixSb.toString() + file.readLine();
 
 			// This is absurd.
@@ -60,12 +65,28 @@ public class ErrorCenter {
 			for (int i = 0; i < TAB_SIZE; i++) {
 				spaces += " ";
 			}
-			out = out.replace("\t", spaces);
+			// Replace each tab with TAB_SIZE spaces, but also track how much to
+			// decrement the col by. This is because col is calculated with the
+			// assumption that each tab took up ANTLR_TAB_SIZE; reason why is
+			// not known.
+			StringBuilder outBuilder = new StringBuilder();
+			char c;
+			for (int i = 0; i < out.length(); i++) {
+				c = out.charAt(i);
+				if (c == '\t') {
+					outBuilder.append(spaces);
+					col -= (ANTLR_TAB_SIZE - TAB_SIZE);
+				} else {
+					outBuilder.append(c);
+				}
+			}
+			out = outBuilder.toString();
 			System.out.println(out);
 
+			// Add the number of spaces necessary for caret.
 			StringBuilder caretSb = new StringBuilder();
-			int end = sizeWithTabs(out, TAB_SIZE, col + prefixSb.length() - 1);
-			for (int i = 0; i < end; i++) {
+			// col already takes TAB_SIZE into account.
+			for (int i = 0; i < prefixSb.length() + col - 1; i++) {
 				caretSb.append(" ");
 			}
 			caretSb.append("^");
@@ -82,12 +103,15 @@ public class ErrorCenter {
 	 * @param tabsize
 	 * @return
 	 */
-	private static int sizeWithTabs(String in, int tabsize, int col) {
+	private static int sizeWithTabs(String in, int col) {
+		System.out.println("col is " + col);
+		System.out
+				.println("WORKING WITH `" + in + "` of length " + in.length());
 		int size = 0;
 		for (int i = 0; i < col; i++) {
 			char c = in.charAt(i);
 			if (c == '\t') {
-				size += tabsize;
+				size += TAB_SIZE;
 			} else {
 				size += 1;
 			}
