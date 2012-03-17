97a29bcbdcea9bccdb263700344947c83a4cb213
Joshua Ma <me@joshma.com>
2012-03-17 02:50:30 -0400

    printing helper method works

--- src/edu/mit/compilers/codegen/AsmVisitor.java
+++ src/edu/mit/compilers/codegen/AsmVisitor.java
@@ -1,10 +1,13 @@
 package edu.mit.compilers.codegen;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import edu.mit.compilers.codegen.asm.ASM;
+import edu.mit.compilers.codegen.asm.LabelASM;
 import edu.mit.compilers.codegen.asm.OpASM;
 import edu.mit.compilers.codegen.asm.OpCode;
 import edu.mit.compilers.codegen.asm.SectionASM;
@@ -16,6 +19,7 @@ public class AsmVisitor {
 	// Static variable because Strings have to be added to it from within other
 	// code.
 	private static List<ASM> dataSection;
+	private static Set<String> externCalls = new HashSet<String>();
 
 	private AsmVisitor(MidSymbolTable symbolTable) {
 	}
@@ -23,7 +27,7 @@ public class AsmVisitor {
 	public static String generate(MidSymbolTable symbolTable) {
 
 		List<ASM> asm = new ArrayList<ASM>();
-		
+
 		asm.addAll(createBSSSection(symbolTable));
 		dataSection = createDataSection(symbolTable);
 		List<ASM> textSection = createTextSection();
@@ -36,6 +40,9 @@ public class AsmVisitor {
 		asm.addAll(textSection);
 
 		StringBuilder out = new StringBuilder();
+		for (String extern : externCalls) {
+			out.append(new OpASM(OpCode.EXTERN, extern).toString());
+		}
 		for (ASM asmLine : asm) {
 			out.append(asmLine.toString());
 		}
@@ -65,7 +72,7 @@ public class AsmVisitor {
 		}
 		return out;
 	}
-	
+
 	private static List<ASM> createDataSection(MidSymbolTable symbolTable) {
 		List<ASM> out = new ArrayList<ASM>();
 		out.add(new SectionASM("data"));
@@ -78,5 +85,38 @@ public class AsmVisitor {
 		out.add(new OpASM(OpCode.GLOBAL, "main"));
 		return out;
 	}
-	
+
+	private static int stringLabelCount = 0;
+
+	private static String labelSafeString(String text) {
+		String clean = text.replaceAll("[^0-9A-Za-z]", "");
+		return "s_" + clean.substring(0, Math.min(8, clean.length()))
+				+ stringLabelCount++;
+	}
+
+	public static List<ASM> generatePrintln(String text) {
+		externCalls.add("printf");
+		
+		String labelText = labelSafeString(text);
+		dataSection.add(new LabelASM("", labelText));
+		// asciiBuilder converts all strings to comma-separated list of ascii to
+		// escape all text input.
+		StringBuilder asciiBuilder = new StringBuilder();
+		String prefix = "";
+		for (char c : text.toCharArray()) {
+			asciiBuilder.append(String.format(prefix + "%d", (int) c));
+			prefix = ",";
+		}
+		// Add newline and NULL byte.
+		asciiBuilder.append(prefix + "10,0");
+		dataSection.add(new OpASM("`" + text + "`", OpCode.DB, asciiBuilder.toString()));
+		
+		List<ASM> out = new ArrayList<ASM>();
+		out.add(new OpASM(String.format("start of printf %s", text),
+				OpCode.MOV, Reg.RDI.name(), labelText));
+		out.add(new OpASM(OpCode.MOV, Reg.RAX.name(), "0"));
+		out.add(new OpASM(OpCode.CALL, "printf"));
+		return out;
+	}
+
 }
