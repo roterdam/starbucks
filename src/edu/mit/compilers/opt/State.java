428cb0a93f8ec03a0d39b5d5ef5d15f83d58e437
Joshua Ma <me@joshma.com>
2012-04-05 19:37:52 -0400

    initial and bottom methods for state interface

--- src/edu/mit/compilers/opt/State.java
+++ src/edu/mit/compilers/opt/State.java
@@ -2,4 +2,13 @@ package edu.mit.compilers.opt;
 
 public interface State<T> {
 
+	/**
+	 * The initial state in the analysis. Note that this can either be at the
+	 * start of the program or the end, depending on whether it's forward or
+	 * backwards.
+	 */
+	public State<T> getInitialState();
+	
+	public State<T> getBottomState();
+
 }
--- src/edu/mit/compilers/opt/cse/CSEState.java
+++ src/edu/mit/compilers/opt/cse/CSEState.java
@@ -4,4 +4,18 @@ import edu.mit.compilers.opt.State;
 
 public class CSEState implements State<CSEState> {
 
+	@Override
+	public State<CSEState> getInitialState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public State<CSEState> getBottomState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
 }
