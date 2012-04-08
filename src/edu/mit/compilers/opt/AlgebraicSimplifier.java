ba238f47c68d33d389a5dd8eb043847b1a206605
Saif Hakim <saif@mit.edu>
2012-04-08 15:05:05 -0400

    almost done.

--- src/edu/mit/compilers/opt/AlgebraicSimplifier.java
+++ src/edu/mit/compilers/opt/AlgebraicSimplifier.java
@@ -28,7 +28,6 @@ import edu.mit.compilers.grammar.tokens.TRUENode;
 
 // TODO(saif): move UnaryMinusNode clean into here.
 
-// TODO(saif): inherit list of things to do.
 
 // Note: if we replace a node with it's child node, do we need to clear out it's siblings? it should happen already.
 
@@ -68,8 +67,22 @@ public class AlgebraicSimplifier {
 		return node;
 	}
 	
+	@SuppressWarnings("serial")
 	public static ExpressionNode simplifyExpression(BANGNode node) {
-		// TODO implement
+		final BooleanNode opNode = (BooleanNode) node.getOperand().simplify();
+		if(opNode instanceof TRUENode){
+			return new FALSENode(){{
+				setText("false");
+				getCallsBeforeExecution().addAll(opNode.getCallsBeforeExecution());
+				getCallsAfterExecution().addAll(opNode.getCallsAfterExecution());
+			}};
+		}else if(opNode instanceof FALSENode){
+			return new TRUENode(){{
+				setText("true");
+				getCallsBeforeExecution().addAll(opNode.getCallsBeforeExecution());
+				getCallsAfterExecution().addAll(opNode.getCallsAfterExecution());
+			}};
+		}
 		return node;
 	}
 	
@@ -83,25 +96,129 @@ public class AlgebraicSimplifier {
 		return node;
 	}
 	
-	public static ExpressionNode simplifyExpression(CHAR_LITERALNode node) {
-		// TODO implement
-		return node;
+	@SuppressWarnings("serial")
+	public static ExpressionNode simplifyExpression(final CHAR_LITERALNode node) {
+		return new INT_LITERALNode(){{
+			setText(Long.toString(node.getValue()));
+			initializeValue();
+			getCallsBeforeExecution().addAll(node.getCallsBeforeExecution());
+			getCallsAfterExecution().addAll(node.getCallsAfterExecution());
+		}};
 	}
 	
 	public static ExpressionNode simplifyExpression(INT_LITERALNode node) {
-		// TODO implement
 		return node;
 	}
 	
+	@SuppressWarnings("serial")
 	public static ExpressionNode simplifyExpression(DIVIDENode node) {
-		// TODO implement
+
+		// Case 1: ???     / int(0) --> Divide by 0 error
+		// Case 2: expr(x) / int(1) --> expr(x)
+		// Case 3: int(a)  / int(b) --> int(a/b) (b!=0)
+		// Case 4: int(0)  / expr(x) --> 0 but check for divide by expr(x)=0
+
+		final ExpressionNode leftOp = node.getLeftOperand().simplify();
+		final ExpressionNode rightOp = node.getRightOperand().simplify();
+		
+		if(rightOp instanceof INT_LITERALNode){
+			long rightVal = ((INT_LITERALNode) rightOp).getValue();
+			
+			if(rightVal == 0){
+				// Case 1
+				// TODO (ERROR OUT RIGHT NOW)
+			}else if(rightVal == 1){
+				// Case 2
+				leftOp.getCallsAfterExecution().addAll(rightOp.getAllCallsDuringExecution());
+				leftOp.setNextSibling(null);
+				return leftOp;
+			}else if(leftOp instanceof INT_LITERALNode){
+				// Case 3
+				long leftVal = ((INT_LITERALNode) leftOp).getValue();
+				long newVal = leftVal / rightVal;
+				INT_LITERALNode newNode = new INT_LITERALNode();
+				newNode.setText(Long.toString(newVal));
+				newNode.initializeValue();
+				// Update pre-post instructions
+				newNode.getCallsBeforeExecution().addAll(leftOp.getAllCallsDuringExecution());
+				newNode.getCallsBeforeExecution().addAll(rightOp.getCallsBeforeExecution());
+				newNode.getCallsAfterExecution().addAll(rightOp.getCallsAfterExecution());
+				return newNode;
+			}
+		}else if(leftOp instanceof INT_LITERALNode){
+			long leftVal = ((INT_LITERALNode) leftOp).getValue();
+			if(leftVal == 0){
+				return new INT_LITERALNode(){{
+					setText("0");
+					initializeValue();
+					getCallsBeforeExecution().addAll(leftOp.getAllCallsDuringExecution());
+					// TODO : ADD CHECK FOR if evaluate(rightOp) == 0 
+					getCallsAfterExecution().addAll(rightOp.getAllCallsDuringExecution());
+				}};
+			}
+		}
+		
+		node.replaceChild(0, node.getLeftOperand().simplify());
+		node.replaceChild(1, node.getRightOperand().simplify());
 		return node;
 	}
 	
+	@SuppressWarnings("serial")
 	public static ExpressionNode simplifyExpression(MODNode node) {
-		// TODO implement
+
+		// Case 1: ???     % int(0) --> Divide by 0 error
+		// Case 2: expr(x) % int(1) --> expr(x)
+		// Case 3: int(a)  % int(b) --> int(a/b) (b!=0)
+		// Case 4: int(0)  % expr(x) --> 0 but check for divide by expr(x)=0
+
+		final ExpressionNode leftOp = node.getLeftOperand().simplify();
+		final ExpressionNode rightOp = node.getRightOperand().simplify();
+		
+		if(rightOp instanceof INT_LITERALNode){
+			long rightVal = ((INT_LITERALNode) rightOp).getValue();
+			
+			if(rightVal == 0){
+				// Case 1
+				// TODO (ERROR OUT RIGHT NOW)
+			}else if(rightVal == 1){
+				// Case 2
+				return new INT_LITERALNode(){{
+					setText("0");
+					initializeValue();
+					getCallsBeforeExecution().addAll(leftOp.getAllCallsDuringExecution());
+					getCallsAfterExecution().addAll(rightOp.getAllCallsDuringExecution());
+				}};
+				
+			}else if(leftOp instanceof INT_LITERALNode){
+				// Case 3
+				long leftVal = ((INT_LITERALNode) leftOp).getValue();
+				long newVal = Math.abs(leftVal % rightVal); // TODO (this is the behaviour of our mod operator, right?)
+				INT_LITERALNode newNode = new INT_LITERALNode();
+				newNode.setText(Long.toString(newVal));
+				newNode.initializeValue();
+				// Update pre-post instructions
+				newNode.getCallsBeforeExecution().addAll(leftOp.getAllCallsDuringExecution());
+				newNode.getCallsBeforeExecution().addAll(rightOp.getCallsBeforeExecution());
+				newNode.getCallsAfterExecution().addAll(rightOp.getCallsAfterExecution());
+				return newNode;
+			}
+		}else if(leftOp instanceof INT_LITERALNode){
+			long leftVal = ((INT_LITERALNode) leftOp).getValue();
+			if(leftVal == 0){
+				return new INT_LITERALNode(){{
+					setText("0");
+					initializeValue();
+					getCallsBeforeExecution().addAll(leftOp.getAllCallsDuringExecution());
+					// TODO : ADD CHECK FOR if evaluate(rightOp) == 0 
+					getCallsAfterExecution().addAll(rightOp.getAllCallsDuringExecution());
+				}};
+			}
+		}
+		
+		node.replaceChild(0, node.getLeftOperand().simplify());
+		node.replaceChild(1, node.getRightOperand().simplify());
 		return node;
-	}
+	}	
 	
 	public static ExpressionNode simplifyExpression(OpSameSame2BoolNode node) {
 		node.replaceChild(0, node.getLeftOperand().simplify());
@@ -118,10 +235,12 @@ public class AlgebraicSimplifier {
 	}
 	
 	public static ExpressionNode simplifyExpression(OpIntInt2BoolNode node) {
-		// TODO implement
+		node.replaceChild(0, node.getLeftOperand().simplify());
+		node.replaceChild(1, node.getRightOperand().simplify());
 		return node;
 	}
 	
+	@SuppressWarnings("serial")
 	public static ExpressionNode simplifyExpression(ORNode node){
 		final BooleanNode leftNode = (BooleanNode) node.getLeftOperand().simplify();
 		final BooleanNode rightNode = (BooleanNode) node.getRightOperand().simplify();
@@ -169,9 +288,7 @@ public class AlgebraicSimplifier {
 				return newNode;
 			}else if(leftVal == 0){			
 				// Case 2
-				rightOp.getCallsBeforeExecution().addAll(0, leftOp.getCallsBeforeExecution());
-				rightOp.getCallsBeforeExecution().addAll(0, leftOp.getCallsAfterExecution());
-				rightOp.setNextSibling(null);
+				rightOp.getCallsBeforeExecution().addAll(0, leftOp.getAllCallsDuringExecution());
 				return rightOp;
 			}
 		}else if(rightOp instanceof INT_LITERALNode){
@@ -180,8 +297,8 @@ public class AlgebraicSimplifier {
 				// Case 3
 				leftOp.getCallsAfterExecution().addAll(rightOp.getCallsBeforeExecution());
 				leftOp.getCallsAfterExecution().addAll(rightOp.getCallsAfterExecution());
-				rightOp.setNextSibling(null);
-				//leftOp leftOp;
+				leftOp.setNextSibling(null);
+				return leftOp;
 			}
 		}
 		
@@ -190,8 +307,64 @@ public class AlgebraicSimplifier {
 		return node;
 	}
 	
+	@SuppressWarnings("serial")
 	public static ExpressionNode simplifyExpression(TIMESNode node){
-		// TODO implement
+		// Case 1: int(a)  * int(b)  --> int(a*b)
+		// Case 2: int(1)  * expr(x) --> expr(x)
+		// Case 4: int(0)  * expr(x) --> int(0)
+		// Case 3: expr(x) + int(1)  --> expr(x)
+		// Case 5: expr(x) * int(0)  --> int(0)	
+		
+		final ExpressionNode leftOp = node.getLeftOperand().simplify();
+		final ExpressionNode rightOp = node.getRightOperand().simplify();
+		
+		if(leftOp instanceof INT_LITERALNode){
+			long leftVal = ((INT_LITERALNode) leftOp).getValue();
+			if(rightOp instanceof INT_LITERALNode){
+				long rightVal = ((INT_LITERALNode) rightOp).getValue();
+				// Case 1
+				long newVal = leftVal * rightVal;
+				INT_LITERALNode newNode = new INT_LITERALNode();
+				newNode.setText(Long.toString(newVal));
+				newNode.initializeValue();
+				// Update pre-post instructions
+				newNode.getCallsBeforeExecution().addAll(leftOp.getAllCallsDuringExecution());
+				newNode.getCallsBeforeExecution().addAll(rightOp.getCallsBeforeExecution());
+				newNode.getCallsAfterExecution().addAll(rightOp.getCallsAfterExecution());
+				return newNode;
+			}else if(leftVal == 1){			
+				// Case 2
+				rightOp.getCallsBeforeExecution().addAll(0, leftOp.getAllCallsDuringExecution());
+				return rightOp;
+			}else if(leftVal == 0){
+				// Case 3
+				return new INT_LITERALNode(){{
+					setText("0");
+					initializeValue();
+					getCallsBeforeExecution().addAll(leftOp.getAllCallsDuringExecution());
+					getCallsAfterExecution().addAll(rightOp.getAllCallsDuringExecution());
+				}};
+			}
+		}else if(rightOp instanceof INT_LITERALNode){
+			long rightVal = ((INT_LITERALNode) rightOp).getValue();
+			if(rightVal == 1){
+				// Case 4
+				leftOp.getCallsAfterExecution().addAll(rightOp.getAllCallsDuringExecution());
+				leftOp.setNextSibling(null);
+				return leftOp;
+			}else if(rightVal == 0){
+				// Case 5
+				return new INT_LITERALNode(){{
+					setText("0");
+					initializeValue();
+					getCallsBeforeExecution().addAll(rightOp.getAllCallsDuringExecution());
+					getCallsAfterExecution().addAll(leftOp.getAllCallsDuringExecution());
+				}};
+			}
+		}
+		
+		node.replaceChild(0, node.getLeftOperand().simplify());
+		node.replaceChild(1, node.getRightOperand().simplify());
 		return node;
 	}
 }
