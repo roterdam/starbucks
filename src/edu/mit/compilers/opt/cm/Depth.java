af381fbcdc6de5c817ba7fb763bd40d7fda39c2e
Sajith <sajith@mit.edu>
2012-05-13 20:54:28 -0400

    depth class

--- src/edu/mit/compilers/opt/cm/Depth.java
+++ src/edu/mit/compilers/opt/cm/Depth.java
@@ -2,13 +2,27 @@ package edu.mit.compilers.opt.cm;
 
 public class Depth {
 	
-	int depth;
-
-	public Nesting() {
-		depth = 0;
+	int value;
+	
+	public Depth() {
+		this.value = 0;
 	}
 	
 	public void increase() {
-		depth++;
+		this.value++;
 	}
+	
+	public int getDepth() {
+		return this.value;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (!(o instanceof Depth)) {
+			return false;
+		}
+		Depth d = (Depth) o;
+		return d.getDepth() == this.getDepth();
+	}
+	
 }
