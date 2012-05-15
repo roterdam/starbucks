2fdbb2f6498a34a87a838ebabf66bc8812bedc48
Sajith <sajith@mit.edu>
2012-05-15 02:54:43 -0400

    base cases for statements

--- src/edu/mit/compilers/opt/cm/CMTransformer.java
+++ src/edu/mit/compilers/opt/cm/CMTransformer.java
@@ -1,14 +1,51 @@
 package edu.mit.compilers.opt.cm;
 
+import edu.mit.compilers.codegen.nodes.MidNode;
+import edu.mit.compilers.codegen.nodes.MidSaveNode;
+import edu.mit.compilers.codegen.nodes.regops.MidArithmeticNode;
+import edu.mit.compilers.codegen.nodes.regops.MidLoadNode;
+import edu.mit.compilers.codegen.nodes.regops.MidNegNode;
+import edu.mit.compilers.codegen.nodes.regops.MidRegisterNode;
+import edu.mit.compilers.opt.Analyzer;
 import edu.mit.compilers.opt.Block;
 import edu.mit.compilers.opt.Transformer;
+import edu.mit.compilers.opt.regalloc.LivenessDoctor;
+import edu.mit.compilers.opt.regalloc.LivenessState;
 
 public class CMTransformer extends Transformer<CMState> {
 	
+	Analyzer<LivenessState, LivenessDoctor> liveness;
+	
+	public CMTransformer(
+			Analyzer<LivenessState, LivenessDoctor> livenessAnalyzer) {
+		this.liveness = livenessAnalyzer;
+	}
+
 	@Override
 	protected void transform(Block block, CMState state) {
-		// TODO Auto-generated method stub
+		CMState local;
+		if (state == null) {
+			local = new CMState();
+		} else {
+			local = state.clone();
+		}
 		
+		if (state.getLoop(block) == null) {
+			return;
+		}
+		
+		for (MidNode node : block) {
+			if (node instanceof MidSaveNode && ((MidSaveNode) node).savesRegister()) {
+				MidRegisterNode reg = (MidRegisterNode) ((MidSaveNode) node).getRegNode();
+				if (reg instanceof MidArithmeticNode) {
+					
+				} else if (reg instanceof MidNegNode) {
+					
+				} else if (reg instanceof MidLoadNode) {
+					
+				}
+			}
+		}
 	}
 
 }
